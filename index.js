31545fe1f3db3eb82f7b1360269567597e795629
Tigran Vardanyan <tigran.vardanyan@x-tech.am>
2024-09-10 17:30:38 +0400

    New Link

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.on("callback_query", function (query) {
         bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
     } else {
         queries[query.id] = query;
-        let gameurl = "https://github.com/Tigran-Vardanyan/CanFlipT.git";
+        let gameurl = "https://github.com/Tigran-Vardanyan/CanFlipT";
         bot.answerCallbackQuery({
             callback_query_id: query.id,
             url: gameurl
