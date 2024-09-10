8768a7bfaf664b7ad43b20d96da75295495aacb4
Tigran Vardanyan <tigran.vardanyan@x-tech.am>
2024-09-10 18:36:34 +0400

    Fix Force Charge

--- Build/CanFlipT.loader.js
+++ Build/CanFlipT.loader.js
@@ -55,7 +55,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 62937983,
+    wasmFileSize: 62937840,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
